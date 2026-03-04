829aebd8a65455c6a68984e02f30d37130cd8db5
rksujith <rksujith2005@gmail.com>
2026-03-04 09:03:20 +0530

    Create greater number.c

---
+++ greater number.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    int n,j;
+    scanf("%d%d",&n,&j);
+    if(n>j){
+        printf("%d is greater",n);
+    }
+    else{
+        printf("%d is greater",j);
+    }
+    return 0;
+}
+
+
+
+
+
+
