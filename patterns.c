f25e7352e0eb5757c131fd920ca9706b9164217c
rksujith <rksujith2005@gmail.com>
2026-03-06 22:55:20 +0530

    Create patterns.c

---
+++ patterns.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+    int num=1;
+    for(int i=1;i<=3;i++){
+        for(int j=1;j<=3;j++){
+             printf("%d",num);
+        num++;
+        }
+      printf("\n");
+    }
+  
+    
+    return 0;
+}
